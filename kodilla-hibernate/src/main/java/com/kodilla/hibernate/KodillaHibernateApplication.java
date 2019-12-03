846e1de703193610868cd63710bfe19e6b875317
MiKraska <mikraska@gmail.com>
2019-12-03 20:17:55 +0100

    Zadanie 17.4.2

--- kodilla-hibernate/src/main/java/com/kodilla/hibernate/KodillaHibernateApplication.java
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/KodillaHibernateApplication.java
@@ -9,5 +9,6 @@ public class KodillaHibernateApplication {
     public static void main(String[] args) {
         SpringApplication.run(KodillaHibernateApplication.class, args);
         // Komentarz test
+        // Komentarz 2.0
     }
 }
